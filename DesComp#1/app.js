d67de0fba2b4a8af67d22a83efdfe9557762d977
yael campos <yaelmagdalenacampos@gmail.com>
2022-05-26 15:14:59 -0300

    Primer commit con archivo app.js

---
+++ DesComp#1/app.js
@@ -0,0 +1,7 @@
+let golocina = prompt("Ingrese la golocina");
+
+while(golocina !=="chocolate"){
+    alert("Esa golocina no me gusta")
+    golocina = prompt("Ingrese otra golocina");
+}
+alert("El chocolate que amo!")
