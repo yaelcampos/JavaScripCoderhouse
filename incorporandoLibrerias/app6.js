996a89f9b43dd3f8f4a1ac1fa4b755fd5b620033
yael campos <yaelmagdalenacampos@gmail.com>
2022-07-06 19:08:58 -0300

    Agrego libreria AOS

---
+++ incorporandoLibrerias/app6.js
@@ -0,0 +1,62 @@
+const peliculasContainer = document.querySelector("#card-peliculas")
+
+const mostrarPeliculas = () => {
+    funcion_cine.forEach(pelicula => {
+        const cardPelicula = document.createElement("section")
+        cardPelicula.classList.add("#peliculas")
+
+        peliculas.innerHTML += `
+        <div class="card card-style my-4" style="width: 18rem;">
+        <img src="${pelicula.imagen}" class="card-img-top img-cartelera" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${pelicula.nombre}</h5>
+        <p class="card-text">${pelicula.descripcion}</p>
+        <button id="${pelicula.id}" class="btn-entradas">Comprar entradas</button>
+        </div>
+        </div>
+        `
+    });
+}
+
+
+const modalContainer = document.querySelector("#modal-container")
+const openModal = document.querySelector("btn-entradas")
+const closeModal = document.querySelector("#close-modal")
+
+const seleccionarPelicula = e => {
+    if (e.target.classList.contains('btn-entradas')) {
+        const id = e.target.id;
+        const pelicula = funcion_cine.find(p => p.id == id);
+
+        _div = ` 
+            <h4>Elija su pelicula: ${pelicula.nombre} </h4>
+            <h4>${pelicula.horarios.map((h) =>{ return `<br>${h.dia} ${ h.horarios.map(( hr) => ` <br> ${hr.hora} `  )}`  })}
+            </h4>
+            <br>
+            `
+        modalContainer.querySelector("#my-modal").innerHTML = _div
+
+        modalContainer.classList.add("my-modal-container--visible")
+
+        localStorage.setItem("peliculas",JSON.stringify(pelicula))
+
+        const listaPelicula = JSON.parse(localStorage.getItem("peliculas"))
+
+        console.log(listaPelicula)
+
+    }
+}
+
+closeModal.addEventListener( "click", () => {
+    modalContainer.classList.remove("my-modal-container--visible")
+})
+
+peliculas.addEventListener('click', seleccionarPelicula);
+window.addEventListener('DOMContentLoaded', mostrarPeliculas);
+
+
+
+
+
+
+
---
+++ incorporandoLibrerias/peliculas_json.js
@@ -0,0 +1,230 @@
+const  funcion_cine = [
+    {
+        "id": 1,
+        "nombre": "Jurassic World Dominio",
+        "duraccion" : 190,
+        "clasificacion" : "Ciencia Ficcion",
+        "idioma" : "Subtitulado al español",
+        "imagen": "ImCartelera/jurassic.jpg",
+        "descripcion": "Dinosaurios persiguiendo gente y una historia de amor en el medio.",
+        "horarios" : [
+            {
+                "dia": "Viernes",
+                "horarios" : [
+                    {
+                        "hora": "12:00",
+                        "cantidad": "4"
+                    },
+                    { 
+                        "hora": "17:20",
+                        "cantidad": "6"
+                    }
+                ]
+            },
+            {
+                "dia": "Sabado",
+                "horarios" : [
+                    {
+                        "hora": "16:00",
+                        "cantidad": "3"
+                    },
+                    { 
+                        "hora": "18:30",
+                        "cantidad": "4"
+                    }
+                ]
+            }
+        ],
+        "precio": "$500",
+    },
+
+    {   "id":2,
+        "nombre": "Minions",
+        "duraccion" : 90,
+        "clasificacion" : "Dibujos animados",
+        "idioma" : "Castellano",
+        "imagen": "ImCartelera/minions.jpg",
+        "descripcion": "Bichitos amarillos que hacen ruidos raros.",
+        "horarios" : [
+            {
+                "dia": "Viernes",
+                "horarios" : [
+                    {
+                        "hora": "9:00",
+                        "cantidad": "4"
+                    },
+                    { 
+                        "hora": "13:00",
+                        "cantidad": "6"
+                    }
+                ]
+            },
+            {
+                "dia": "Sabado",
+                "horarios" : [
+                    {
+                        "hora": "15:00",
+                        "cantidad": "3"
+                    },
+                    { 
+                        "hora": "18:45",
+                        "cantidad": "4"
+                    }
+                ]
+            }
+        ],
+        "precio": "$500",
+    },
+
+    {   "id":3,
+        "nombre": "Mi novia Polly",
+        "duraccion" : 110,
+        "clasificacion" : "comedia",
+        "idioma" : "Castellano",
+        "imagen": "ImCartelera/polly.png",
+        "descripcion": "La historia de amor más cliché en la historia del cine.",
+        "horarios" : [
+            {
+                "dia": "Viernes",
+                "horarios" : [
+                    {
+                        "hora": "9:30",
+                        "cantidad": "4"
+                    },
+                    { 
+                        "hora": "11:45",
+                        "cantidad": "6"
+                    }
+                ]
+            },
+            {
+                "dia": "Sabado",
+                "horarios" : [
+                    {
+                        "hora": "18:00",
+                        "cantidad": "3"
+                    },
+                    { 
+                        "hora": "22:30",
+                        "cantidad": "4"
+                    }
+                ]
+            }
+        ],
+        "precio": "$500",
+    },
+
+    {   "id":4,
+        "nombre": "Le temes a la oscuridad",
+        "duraccion" : 110,
+        "clasificacion" : "suspenso",
+        "idioma" : "Subtitulada",
+        "imagen": "ImCartelera/oscuridad.png",
+        "descripcion": "Un clásico, eso es lo que es.",
+        "horarios" : [
+            {
+                "dia": "Viernes",
+                "horarios" : [
+                    {
+                        "hora": "9:20",
+                        "cantidad": "4"
+                    },
+                    { 
+                        "hora": "12:40",
+                        "cantidad": "6"
+                    }
+                ]
+            },
+            {
+                "dia": "Sabado",
+                "horarios" : [
+                    {
+                        "hora": "9:00",
+                        "cantidad": "3"
+                    },
+                    { 
+                        "hora": "13:50",
+                        "cantidad": "4"
+                    }
+                ]
+            }
+        ],
+        "precio": "$500",
+    },
+
+    {   "id":5,
+        "nombre": "Sonic 2",
+        "duraccion" : 90,
+        "clasificacion" : "aventuras",
+        "idioma" : "Subtitulada",
+        "imagen": "ImCartelera/sony-2.png",
+        "descripcion": "El prisas.",
+        "horarios" : [
+            {
+                "dia": "Viernes",
+                "horarios" : [
+                    {
+                        "hora": "9:00",
+                        "cantidad": "4"
+                    },
+                    { 
+                        "hora": "11:45",
+                        "cantidad": "6"
+                    }
+                ]
+            },
+            {
+                "dia": "Sabado",
+                "horarios" : [
+                    {
+                        "hora": "16:00",
+                        "cantidad": "3"
+                    },
+                    { 
+                        "hora": "20:30",
+                        "cantidad": "4"
+                    }
+                ]
+            }
+        ],
+        "precio": "$500",
+    },
+
+    {   "id":6,
+    "nombre": "Avatar 2",
+    "duraccion" : 90,
+    "clasificacion" : "aventuras",
+    "idioma" : "Subtitulada",
+    "imagen": "ImCartelera/Avatar-2.png",
+    "descripcion": "Bichos azules de tres metros.",
+    "horarios" : [
+        {
+            "dia": "Viernes",
+            "horarios" : [
+                {
+                    "hora": "9:00",
+                    "cantidad": "4"
+                },
+                { 
+                    "hora": "11:00",
+                    "cantidad": "6"
+                }
+            ]
+        },
+        {
+            "dia": "Sabado",
+            "horarios" : [
+                {
+                    "hora": "15:00",
+                    "cantidad": "3"
+                },
+                { 
+                    "hora": "20:00",
+                    "cantidad": "4"
+                }
+            ]
+        }
+    ],
+    "precio": "$500",
+},
+]
